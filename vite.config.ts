e7a125886d3321db3b85f8cbc7f04d1fbe503a32
Jenna deBoisblanc <jdeboi@gmail.com>
2025-07-23 14:45:51 -0500

    first commit

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+// vite.config.ts
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/school-transcript-link/', // ← match GitHub repo name
+})
